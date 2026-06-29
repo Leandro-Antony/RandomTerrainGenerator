71d18deb3b42c8995c76979b281e7ba205ee6967
Murilo César <murilo.sirinobc@gmail.com>
2026-06-28 21:28:08 -0300

    Adicionando sombreamento no função gerarTerreno

--- terreno/gerarRelevo.cpp
+++ terreno/gerarRelevo.cpp
@@ -7,13 +7,12 @@ void Terreno::gerarRelevoPPM(const std::string arquivo_cores, const std::string 
     Paleta cores(arquivo_cores);
 
     int numCores = cores.obterTamanho();
-    int divisor = 140/numCores;
+    int divisor = 260/numCores;
     
     int lado = dimensao;
 
     Imagem mapa(lado, lado);
     
-
     for (int i = 0; i < lado; i++) {
         for (int j = 0; j < lado; j++) {
             int altitude = obterAltitude(i, j);
@@ -23,14 +22,34 @@ void Terreno::gerarRelevoPPM(const std::string arquivo_cores, const std::string 
             if (indice >= numCores) indice = numCores - 1;
             
             Cor corAltitude = cores.obterCor(indice);
+
+            bool deveSombrear = false;
+
+            if (i > 0 && j > 0) {
+                if (altitude < obterAltitude(i - 1, j - 1)) {
+                    deveSombrear = true;
+                }
+            } else if (i == 0 && j > 0) {
+                if (altitude < obterAltitude(i, j - 1)) {
+                    deveSombrear = true;
+                }
+            } else if (i > 0 && j == 0) {
+                if (altitude < obterAltitude(i - 1, j)) {
+                    deveSombrear = true;
+                }
+            }
+
+            if (deveSombrear) {
+                corAltitude.r = (int)(corAltitude.r * 0.7);
+                corAltitude.g = (int)(corAltitude.g * 0.7);
+                corAltitude.b = (int)(corAltitude.b * 0.7);
+            }
+
             mapa.setPixel(i, j, corAltitude);
         }
     }
 
     mapa.salvarPPM(arquivo_salvar);
-    
-    
-
 }
 
 int main() {
@@ -39,7 +58,7 @@ int main() {
     std::cin >> n;
     Terreno t(n);
     t.salvarTerreno("terreno.txt");
-    t.gerarRelevoPPM("./paleta/cores.hex", "./mapa.ppm");
+    t.gerarRelevoPPM("../paleta/cores.hex", "./mapa.ppm");
 
     return 0;
 }
